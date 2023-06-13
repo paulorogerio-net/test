c77858da2fbef4dd4d99c32dc976e09cc977c4ef
paulorogerio-net <87511518+paulorogerio-net@users.noreply.github.com>
2023-06-13 16:24:11 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,35 @@
-api_id = "dev_api_id"
-stage_name = "dev_stage_name"
-usage_plan_name = "dev_usage_plan_name"
-api_key_name = "dev_api_key_name"
-quota_limit = 5000
-quota_offset = 2
-quota_period = "WEEK"
-throttle_burst_limit = 200
-throttle_rate_limit = 100
+variable "api_id" {
+  description = "The API ID for the API Gateway"
+}
+
+variable "stage_name" {
+  description = "The name of the stage"
+}
+
+variable "usage_plan_name" {
+  description = "The name for the usage plan"
+}
+
+variable "api_key_name" {
+  description = "The name for the API key"
+}
+
+variable "quota_limit" {
+  description = "The maximum number of requests that can be made in a given time period"
+}
+
+variable "quota_offset" {
+  description = "The number of requests subtracted from the limit for the first time period"
+}
+
+variable "quota_period" {
+  description = "The time period in which the limit applies (DAY, WEEK, MONTH)"
+}
+
+variable "throttle_burst_limit" {
+  description = "The maximum rate limit over a time ranging from one to a few seconds"
+}
+
+variable "throttle_rate_limit" {
+  description = "The steady-state rate limit (average requests per second over an extended period of time)"
+}
